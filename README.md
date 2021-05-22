0608a4cfc8f0f503b42f176122136c4f05ea10ea
DerGoogler <jxhazhungerz@gmail.com>
2021-05-23 01:01:10 +0200

    commit other files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hentai Web
+How to install:
+- `npm install`
+- launch any `localhost`
+
+
+Enjoying the Hentai`s.
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// Regular imports
+import React from "react";
+import ReactDOM from "react-dom";
+import ons from "onsenui";
+import App from "./App";
+
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+ons.ready(function () {
+ ons.platform.select("ios");
+ // build app
+ var mountNode = document.getElementById("app");
+ ReactDOM.render(<App />, mountNode);
+});
