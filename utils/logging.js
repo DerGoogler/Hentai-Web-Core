90c9a170504110668a47e395e42897121d3d9438
Jimmy Bohm <jxhazhungerz@gmail.com>
2021-05-27 01:39:17 -1100

    .

---
+++ utils/logging.js
@@ -0,0 +1,15 @@
+export function print(data) {
+    console.log(data)
+}
+
+export class print {
+    err(data) {
+        console.error(data);
+    }
+    war(data) {
+        console.warn(data);
+    }
+    inf(data) {
+        console.info(data);
+    }
+}
