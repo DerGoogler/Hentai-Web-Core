55afd9288345dc8c13ab7d47057ca57ae7385790
DerGoogler <jxhazhungerz@gmail.com>
2022-01-09 23:57:51 +0100

    Add statusbar color ro network error

--- Android/app/src/main/assets/www/test.js
+++
@@ -1,9 +0,0 @@
-//var http = require('http');
-console.log("process.argv[2]");
-/*
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end("Hello from Node.js");
-});
-
-server.listen(6060);*/
