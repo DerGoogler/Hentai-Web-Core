e45c7800a9016821f1f2e470cc88f908586d508d
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-02-26 13:24:02 +0100

    Update editor.d.ts

--- dist/misc/editor.d.ts
+++ dist/misc/editor.d.ts
@@ -20,6 +20,12 @@ declare function require(path: any): void;
 // @ts-ignore
 declare const __dirname: string;
 
+declare class ipc {
+  static on(event: string, callback: (...props: any) => void): void;
+  static send(event: string, data: any): void;
+  static destroy(event: string, callback: () => void): void;
+}
+
 interface PluginOptions {
   /**
    * Requires an selected version above {version}
